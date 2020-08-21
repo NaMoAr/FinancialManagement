f3d83a4d8ec5801824aa353fc29c621d09f0e89f
NaMoAr <najmeharani@gmail.com>
2020-08-20 21:37:37 -0700

    Resolved the bug for writing transactions in the file

--- header/CheckingAccount.hpp
+++ header/CheckingAccount.hpp
@@ -1,6 +1,6 @@
 #ifndef CHECKINGACCOUNT_HPP
 #define CHECKINGACCOUNT_HPP
-#include "Account.h"
+#include "Account.hpp"
 using namespace std;
 
 class CheckingAccount : public Account
@@ -8,14 +8,14 @@ class CheckingAccount : public Account
 
 
 public:
-	 CheckingAccount();
-	 CheckingAccount(double initialBalance, string userID);
+	 
+	 CheckingAccount(double initialBalance, int userID);
 	 ~CheckingAccount();
 
 	 stringstream* deposit(double depMoney,stringstream* s);
 	 stringstream* withdraw(double depMoney, stringstream* s);
 	 void transfer(double tranmoney, string giverID, string recieverID);
-     void accountHistory();
+         void accountHistory();
 	 
 
 };
