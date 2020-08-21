7f17da766938db8b345b88d1feb7d99159887154
NaMoAr <najmeharani@gmail.com>
2020-08-21 15:55:22 -0700

    Implemented budjet planning

--- src/CheckingAccountProxy.cpp
+++ src/CheckingAccountProxy.cpp
@@ -34,18 +34,34 @@ stringstream* CheckingAccountProxy::deposit(double depMoney, stringstream* ss)
 
 stringstream* CheckingAccountProxy::withdraw(double withMoney, stringstream* ss)
 {
-	balance = getBalance() - withMoney;
-	if (withMoney > balance) {
+        if (withMoney > balance) {
 		cout << "Sorry, this amount of money is over your balance."<<endl;
 	}
+	else if (balance - withMoney < monthlyHomeRent) {
+		cout << "Sorry, you have to keep your balance greater than $"<<monthlyHomeRent<<" to pay your monthly home rent. Are you sure you still want to do this transacrion?(y/n)" << endl;
+		string yn = "";
+		cin >> yn;
+		if (yn == "y") {
+			balance = getBalance() - withMoney;
+			s << "Checking account, $" << withMoney << " has been withdrawn, " << "New Balance: $" << balance << endl;
+			ss = &s;
+			cout << withMoney << " dollors has been withdrawn" << endl;
+			return ss;
+		}
+		if (yn == "n") {
+			cout << "The transaction heve been canceled." << endl;
+		}
+	}
 	else {
+		balance = getBalance() - withMoney;
 		s << "Checking account, $" << withMoney << " has been withdrawn, " << "New Balance: $" << balance << endl;
 		ss = &s;
 		cout << withMoney << " dollors has been withdrawn" << endl;
 		return ss;
 	}
-	
 }
+	
+
 
 void CheckingAccountProxy::logOut(string depOrwith, double Money, stringstream* ss)
 {
@@ -76,3 +92,17 @@ void CheckingAccountProxy::accountHistory()
 {
 	ptr->accountHistory();
 }
+
+
+void CheckingAccountProxy::setMonthlyHomeRent(double homeRent)
+{
+	monthlyHomeRent = homeRent;
+}
+
+double CheckingAccountProxy::getMonthlyHomeRent()
+{
+	return monthlyHomeRent;
+}
+
+
+
