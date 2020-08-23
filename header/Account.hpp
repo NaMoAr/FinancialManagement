d2c8e91ced494fa139608eed46ebedef84cdb53c
NaMoAr <najmeharani@gmail.com>
2020-08-22 22:14:26 -0700

    Removed unnecessary functions

--- header/Account.hpp
+++ header/Account.hpp
@@ -10,17 +10,15 @@ class Account
 protected:
 double balance = 0.0 ;
 int userID = 0;
+private:
 string password = "";
 		
 
 public:
  Account() {};
- Account(double initialBalance, string userID) {};
  virtual ~Account() {};
- virtual stringstream* deposit(double depMoney, stringstream* s) = 0;
- virtual stringstream* withdraw(double eithMoney, stringstream* s) = 0;
- virtual void transfer(double tranMoney, string giverID, string recieverID)=0;
  virtual void accountHistory() = 0;
+ virtual void logOut(stringstream* ss) = 0;
 
 
 
