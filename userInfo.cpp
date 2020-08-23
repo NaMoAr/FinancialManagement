2a84c5c27aff970f8ea6fc47982b870fa0579f22
loveechloee <pinkberry2444@gmail.com>
2020-08-23 12:56:36 -0700

    Implemented new functions SaveInfo and ReadList

--- userInfo.cpp
+++ userInfo.cpp
@@ -1,4 +1,3 @@
-#include "customerList.hpp"
 #include "userInfo.hpp"
 
 UserInfo::UserInfo() {
@@ -31,19 +30,11 @@ UserInfo::UserInfo() {
 	}
 	inFS.close();
 
-	for (int i = 0; i < id.size(); ++i) {
-		//Calls Account to create Account objects
-		profile = new Profile(); // or new Account();
-		profile->setID(id.at(i));
-		profile->setPassword(pw.at(i));
-		profile->setBalance(stod(balance.at(i)));
-		if(acType.at(i) == "s") {
-			profile->setStocks(stod(funds.at(i)));
-		}
-		else if(acType.at(i) == "c") {
-			profile->setMonthlyHomeRent(stod(funds.at(i)));
-		}
-	}
+	v.at(0) = acType;
+	v.at(1) = id;
+	v.at(2) = pw;
+	v.at(3) = balance;
+	v.at(4) = funds;
 }
 
 UserInfo::~UserInfo() {
@@ -59,16 +50,16 @@ UserInfo::~UserInfo() {
 	delete profile;
 }
 
-void UserInfo::SaveInfo(Account* p) {
+void UserInfo::SaveInfo(int ID, string pass, double b, double f) {
 	for (int i = 0; i < id.size(); ++i) {
-		if (id.at(i) == p->getID()) {
-			balance.at(i) = to_string(p->getBalance());
-			if(acType.at(i) == "s") {
-				funds.at(i) = to_string(p->getUserStocks());
-			}
-			else if (acType.at(i) == "c") {
-				funds.at(i) = to_string(p->getMonthlyHomeRent());
-			}
+		if (to_string(ID) == id.at(i)) {
+			pw.at(i) = pass;
+			balance.at(i) = to_string(b);
+			funds.at(i) = to_string(f);
 		}
 	}
+}
+
+vector<vector<string>> UserInfo::ReadList() {
+	return v;
 }
--- userInfo.hpp
+++ userInfo.hpp
@@ -8,27 +8,31 @@ using namespace std;
 #include <vector>
 #include <sstream>
 
-class UserInfo : public Account {
-	private:
-		Account* profile;
-
+class UserInfo {
 	public:
-		UserInfo();
-		//constructor that reads in UserInfo.txt and loads all user info on list
+		UserInfo(); //constructor that reads in UserInfo.txt and loads all user info on list
 		
-		~UserInfo();
-	
-		virtual void PrintList() {};
-		//updates all user info into UserInfo.txt when customer selects sign out
+		~UserInfo(); //destructor
 
-		void SaveInfo(Account* p);
+		void SaveInfo(int ID, string pass, double b, double f); //called by Account() upon signing out to save new changes
+
+		vector<vector<string>> ReadList(); //called by AccountList to pass over all info read in from list
+
+		vector<vector<string>> v; //variable for ReadList()
+
 
 	protected:
-		vector<string> acType;
-		vector<string> id;
-		vector<string> pw;
+		vector<string> acType; //classifies as c or s for checking or savings
+
+		vector<string> id; //user id
+		
+		vector<string> pw; //password
+
 		vector<string> balance; //current balance in account
+
 		vector<string> funds; //monthlyrent for checkings, stocks for savings
+
+		virtual void PrintList() = 0; //later called by CSVSave and TXTSave subclasses
 };
 
 #endif
