05154e41e642bf2b8d9418340a75d4ceaca1b97e
NaMoAr <najmeharani@gmail.com>
2020-08-22 22:22:24 -0700

    Added the related functions for stock features

--- header/SavingAccountProxy.hpp
+++ header/SavingAccountProxy.hpp
@@ -9,18 +9,25 @@ class SavingAccountProxy : public Account
 private:
 	SavingAccount* ptr;
 	stringstream s;
-
+        int stockNum = 0 ;
+	double stockPrice = 0.0;
 public:
 	
 	SavingAccountProxy(double initialBalance, int userID);
 	~SavingAccountProxy();
 
 	stringstream* deposit(double depMoney, stringstream* ss);
-	void logOut(string depOrwith, double Money, stringstream* ss);
+	void logOut(stringstream* ss);
 	stringstream* withdraw(double withMoney, stringstream* ss);
-	void transfer(double tranmoney, string giverID, string recieverID);
-	double getBalance();
+        double getBalance();
 	void accountHistory();
+        double fetchStockPrice();
+        void calculateProfit();
+        void buyStock();
+        double getStockNum();
+        double getStockPrice();
+        void sellStock();
+	void currentUserStock();
 
 };
 
