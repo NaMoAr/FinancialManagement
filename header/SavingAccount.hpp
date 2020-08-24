44cf1f0ecb07530b7fcce14f710c805f00e07889
NaMoAr <najmeharani@gmail.com>
2020-08-23 20:43:24 -0700

    Added password and id functions

--- header/SavingAccount.hpp
+++ header/SavingAccount.hpp
@@ -8,14 +8,23 @@ using namespace std;
 class SavingAccount : public Account
 {
 
-
+private:
+	int stockNum = 0;
+	double stockPrice = 0.0;
 public:
 	
-	SavingAccount(double initialBalance, int userID);
+	SavingAccount(double initialBalance, int userID,string pass, int stochShareNum, double stockSharePrice);
 	~SavingAccount();
         void logOut(stringstream* ss);
 	void accountHistory();
-
+	double getBalance();
+	void setPassword(string pass);
+	string getPassword();
+	int getID();
+	void setStockNum(int stockShareNum);
+	double getStockNum();
+	void setStockPrice(double stockSharePrice);
+	double getStockPrice();
 
 };
 
