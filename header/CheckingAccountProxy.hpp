967869dc9abd3d77e77d192b244514b3d6a11b57
NaMoAr <najmeharani@gmail.com>
2020-08-23 20:45:09 -0700

    Added password and id functions

--- header/CheckingAccountProxy.hpp
+++ header/CheckingAccountProxy.hpp
@@ -12,14 +12,17 @@ private:
         double monthlyHomeRent = 0.0; 
 public:
 	
-	CheckingAccountProxy(double initialBalance, int userID);
+	CheckingAccountProxy(double initialBalance, int userID, string pass, double homeRent);
 	~CheckingAccountProxy();
 
 	stringstream* deposit(double depMoney, stringstream* ss);
-        void logOut(stringstream* ss);
         stringstream* withdraw(double withMoney, stringstream* ss);
-        double getBalance();
+	void logOut(stringstream* ss);
         void accountHistory();
+	double getBalance();
+	void setPassword(string pass);
+	string getPassword();
+	int getID();
         void setMonthlyHomeRent(double homeRent);
         double getMonthlyHomeRent();
 
