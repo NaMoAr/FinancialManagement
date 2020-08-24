97aebecb42026a2aedb88d9c93ffa3bd03e04517
dchan066-862012834 <dchan066@ucr.edu>
2020-08-23 19:23:01 -0700

    implemented several functions, added adminMenu() and interface(), retyped logIn() to void to accomodate the addition of menus, reorganized public and private methods to enforce encapsulation

--- accountList.cpp
+++ accountList.cpp
@@ -1,14 +1,37 @@
 #include "AccountList.hpp"
+#include <typeinfo>
+
+// TODO: verify the function and implementation of utilized unordered_map functions associated to the object "list"
 
 AccountList::AccountList () {
 // call Chloe's class to read the database file
-// Chloe should call the account constructor to create the accounts and add their pointers to the list
+auto v = object->ReadList(); //TODO: we still need the name of chloe's userifo object
+
+	for (int i = 0, i < v.size(); i++){
+		if (v[i][0] == "s"){ // this implementation of element access for a vecotr of vecotrs of strings may need review
+			Account* temp = new SavingAccountProxy(v[i][3], v[i][1], v[i][2], v[i][4], 0);	
+			list.insert(v[i][1],temp);
+		}
+		else {
+			Account* Temp = new CheckingAccountProxy(v[i][3], v[i][1], v[i][2], v[i][4]);
+			list.insert(v[i][1],Temp);
+		}
+	}
 }
 
 AccountList::~AccountList() {
 // call Chloe's class to write the account data to the database file
+
+
 // delete all pointers in the hash map
-// delete the AccountList and terminate the program
+	for (auto x : list) {
+		if (typeid(x.second).name() == CheckingAccountProxy*)
+			object->saveinfo(c,x.second->getID(), x.second->getPassword(), x.second->getBalance, x.second->getMonthlyHomeRent() ); //TODO: figure out the name of chloe's userinfo object
+		if (typeid(x.second).name() == SavingAccountProxy*)
+			object->saveinfo(c,x.second->getID(), x.second->getPassword(), x.second->getBalance, x.second->getStockNum() );
+		delete x.second();
+	} //once objects in memory are deleted we can close the program
+delete this; // this might be overkill or could somehow break the program? not realy sure
 }
 
 
@@ -18,58 +41,114 @@ AccountList* AccountList::GetInstance() {
 // return the pointer of the object you just created
 }
 
+void interface() {
+	char temp = 4;
+	std::cout << "Welcome to the financial planning and stock management account access tool" << endl << "to access administrator functions enter '0', to log in to a user account enter '2' to quit the program enter '3' " << endl;
+	if (temp == 1) 	adminMenu();
+	if (temp == 2)	login();
+	if (temp != 3)	interface();
+	// final saves to the file calling chloes stuff?
+	return;
+	
+}
+
 void AccountList::createAccount () {
 	std::string temp = "";
 	std::string pass = "";
+	int value;
 	// verify admin status with an admin password
 	std::cout << "Verify administrator status" << endl;
 	std::cin >> temp;
-	if (temp != AdminAccess) {cout << "Verification failed, terminating operation"<<endl; return;
+	if (temp != AdminAccess) {cout << "Verification failed, terminating operation"<<endl; return;}
 	// request new password
 	std::cout << "Assign a password"<<endl;
 	std::cin >> pass;
 	std::cout <<"The password to be assigned is "<< pass << endl; 
 	// generate a unique ID
 	int ID = 0;
-	//ID =  TODO: generation method;
+	ID =  list.size()+1; // ignore account 0 which is reserved for admin access
 	std::cout << "account ID generated: " << ID << endl;
+	// specify initial deposit
+	std::cout << "specify initial deposit amount" << endl;
+	std cin >> value;
+	std:: cout << "the account will be initialized with an inital balance of " << value << end;
 	// assign a type for the account
 	std::cout << "specify account type: 'c' for checking and 's' for savings"
 	char flag = ' ';
 	cin >> flag;
 	// create an account object
 	// fit the pointer into the map
-	}if (flag != 'c' || flag != 's'){
+	if (flag != 'c' && flag != 's'){
 		cout << "Account type invalid, terminating" << endl; return;
 	}
 	if (flag = 'c') {
-		//TODO: construct and store a checking account object
+		// construct and store a checking account object
+		Account* toAdd = new CheckingAccountProxy(value,ID,pass,0,0);
+		list.insert(ID, toAdd);
+		return;
 	}
-	// TODO:  construct and store a saving account object
+	// construct and store a saving account object
+	Account* toadd = new SavingAccountProxy(value,ID,pass,0);
+	list.insert(ID, toadd);
+	return;
 }
 
 void AccountList::deleteAccount () {
-// verify admin status with an admin password
-// search for id, report an error if it doesn't exist 
-// delete the object and set the pointer to null
+	Account* toRemove;
+	int temp = ""
+	// verify admin status with an admin password
+	std::cout << "Verify administrator status" << endl;
+	std::cin >> temp;
+	if (temp != AdminAccess) {cout << "Verification failed, terminating operation"<<endl; return;}	
+	// search for id, report an error if it doesn't exist 
+	std::cout << "specify account ID to remove" << endl;
+	cin >> temp;
+	toRemove = traverseDatabase();
+	if (toRemove == NULL) {std::cout<< "account not found, terminationg operation" << endl; return;}
+	list.erase(ID); 
+	delete toRemove; // may be an incorrect deletion type
+	return;
+	// delete the object and set the pointer to null
 }
 
 // a helper function to let people search the list
-void AccountList::traverseDatabase () {  // Final implementation will return an account*
-// this function searches the database for a given id returning it
-// if none exists it will return NULL
+Account* AccountList::traverseDatabase (int ID) {  // Final implementation will return an account*
+	Account* temp = NULL;
+	// this function searches the database for a given id returning it
+	temp = list.find(ID);
+	// if none exists it will return NULL
+	return temp;
 }
 
-Account* AccountList::logIn() {
-// prompt ID & Password
-// scan list for ID returning null on a fail to find and print "account not found"
-// compare Password returning the account pointer on a match and null on a mismatch, in the fail case print "password incorrect" 
+void AccountList::logIn() {
+	int ID = 0;
+	string pass = "";
+	// prompt ID & Password
+	std::cout << "Enter ID" << endl;
+	std:: cin >> ID;
+	// scan list for ID returning null on a fail to find and print "account not found"
+	userPointer = traverseDatabase(ID);
+	// compare Password returning the account pointer on a match and null on a mismatch, in the fail case print "password incorrect" 
+	if (userPointer == NULL) {std::cout<<"account not found, terminating"; return;}
+	if (userPointer->getPassword() != pass) {std::cout << "password invalid, terminating"; return;}
+	userPointer->Menu(); // this function call will depend on the way Najmeh implements her menu
 }
 
+void adminMenu() {
+char temp
+std::cout << "1 to remove an account. 2 to create an account, any other character to exit admin"<< endl;
+cin >> temp;
+if (temp == '1') deleteAccount();
+if (temp == '2') createAccount();
+return;
+
+}
 
 void AccountList::logOut() {
-// call relevant account history proxy update functions
+// is called by account history proxy logout function
 // unset user pointer
-// re call the log in prompt  
+userPointer = NULL;
+// re call the log in prompt
+logIn();  
 }
 
--- accountList.hpp
+++ accountList.hpp
@@ -21,23 +21,27 @@ class AccountList {
 	
 		std::string adminAccess = "96a~eU=tWyVkz'4V"; // a randomly generated 16 character password to verify an admin before allowing access to admin functions
 
+		void adminMenu(); // a function that helps admins navigate creation and destruction of accounts					Accessed by logIn
+
+		void createAccount(); //  a function for admins to create new accounts in the list						Accessed by adminMenu
+		
+		void deleteAccount(); // a function for admins to delete accounts within the list						Accessed by adminMenu
+		
+		void traverseDatabase(int ID); // a helper function that allows both admins and users to search the list for a given account	Helper for login, delete and create
+
+		void logIn(); //a function that allows users to sign into their accounts.							Accessed by interface?
+	
 	public:
 
-		AccountList(AccountList &other) = delete// we can't let the object be copyable
+		AccountList(AccountList &other) = delete// we can't let the object be copyable							MUST BE PUBLIC
 
-		void operator= (const AccountList &) = delete // we con't let the object be assignable
+		void operator= (const AccountList &) = delete // we con't let the object be assignable						MUST BE PUBLIC
 	
-		AccountList* GetInstance(); // the public interface which manages the creation of the AccountList object
+		AccountList* GetInstance(); // the public interface which manages the creation of the AccountList object			MUST BE PUBLIC
 
-		void createAccount(); //  a function for admins to create new accounts in the list
-		
-		void deleteAccount(); // a function for admins to delete accounts within the list
-		
-		void traverseDatabase(); // a helper function that allows both admins and users to search the list for a given account
+		void interface(); // the public interface that allows a user to log in or quit the program					MUST BE PUBLIC
 
-		Account* logIn(); //a function that allows users to sign into their accounts.
-	
-		void logOut(); // unsets the user pointer and reprompts log in
+		void logOut(); // unsets the user pointer and reprompts log in									MUST BE PUBLIC - called by proxy
 };
 
 #endif
