2b6ed956d0a3778fe804d2b4ac055e4b09318fa2
Chloe Truong <pinkberry2444@gmail.com>
2020-08-26 12:52:24 -0700

    added files for strategy pattern

---
+++ txtSave.hpp
@@ -0,0 +1,22 @@
+#ifndef __TXTSAVE_HPP__
+#define __TXTSAVE_HPP__
+
+#include "userInfo.hpp"
+
+class TXTSave : public UserInfo {
+	public:
+		TXTSave() : UserInfo() {};
+
+		~TXTSave();
+
+		void PrintList(); //Print all information into TXT file
+
+		void SaveInfo(string ac, int ID, string pass, double b, double f);// Called by CustomerList to store all Checking Account info
+
+		void SaveInfo(string ac, int ID, string pass, double b, double f, double n); //Called by CustomerList to store all Savings Account info
+
+		vector<vector<string>> ReadList(); // Called by CustomerList, reads in info from TXT file
+
+		string PrintDebug(); // function for googletest
+};
+#endif //__TXTSAVE_HPP__
