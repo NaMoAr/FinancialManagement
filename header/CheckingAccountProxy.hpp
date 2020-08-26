1c4f6ae5dcd3b2b825ee66f0afea88752dd69600
NaMoAr <najmeharani@gmail.com>
2020-08-26 14:22:29 -0700

    Added CheckingAccountProxy.hpp file into the CrossTest branch

---
+++ header/CheckingAccountProxy.hpp
@@ -0,0 +1,42 @@
+/*
+ * A proxy class of proxy design pattern consisting of 
+ * CheckingAccountProxy's functions
+ * @auther Najmeh M.A
+ */
+
+#ifndef CHECKINGACCOUNTPROXY_HPP
+#define CHECKINGACCOUNTPROXY_HPP
+#include "Account.hpp"
+#include "CheckingAccount.hpp"
+
+class CheckingAccountProxy : public Account
+{
+
+private:
+	CheckingAccount* ptr;
+	stringstream s;
+        double monthlyHomeRent = 0.0; 
+public:
+        CheckingAccountProxy(){};
+	CheckingAccountProxy(double initialBalance, int userID, string pass, double homeRent);
+	~CheckingAccountProxy();
+
+	stringstream* deposit(double depMoney);
+        stringstream* withdraw(double withMoney);
+	void logOut(stringstream* ss);
+        bool accountHistory();
+	void setBalance(double bl);
+	double getBalance();
+	void setPassword(string pass);
+	string getPassword();
+	int getID();
+        void setMonthlyHomeRent(double homeRent);
+        double getMonthlyHomeRent();
+
+
+
+
+};
+
+#endif // CHECKINGACCOUNTPROXY_HPP
+
