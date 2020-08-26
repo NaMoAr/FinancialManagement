8d4ca40e202f8091e7688fb234415ec33cae02dd
NaMoAr <najmeharani@gmail.com>
2020-08-26 14:27:37 -0700

    Moved csvSave.hpp file into the header file

---
+++ header/csvSave.hpp
@@ -0,0 +1,22 @@
+#ifndef __CSVSAVE_HPP__
+#define __CSVSAVE_HPP__
+
+#include "userInfo.hpp"
+
+class CSVSave : public UserInfo {
+	public:
+		CSVSave() : UserInfo() {};
+
+		~CSVSave();
+
+		void PrintList(); //Print all information into CSV file
+
+		void SaveInfo(string ac, int ID, string pass, double b, double f); //Called by CustomerList to store Checking Account Info
+
+		void SaveInfo(string ac, int ID, string pass, double b, double f, double n); //Called by CustomerList to store Savings Account Info
+
+		vector<vector<string>> ReadList(); //Called by CustomerList, reads in info from CSV file
+
+		string PrintDebug(); //function for googletest
+};
+#endif //__CSVSAVE_HPP__
