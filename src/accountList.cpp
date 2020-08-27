5abc63a58e295e628f2bab817259c890b221b792
Chloe Truong <pinkberry2444@gmail.com>
2020-08-27 00:44:37 -0700

    Update accountList.cpp parameterized constructor

--- src/accountList.cpp
+++ src/accountList.cpp
@@ -1,80 +1,87 @@
 #include "../header/accountList.hpp"
-#include "../header/txtSave.hpp"
-#include "../header/csvSave.hpp"
 #include <typeinfo>
 #include <vector>
 using namespace std;
 
 AccountList *AccountList::AccountList_ = 0;
  
-AccountList::AccountList (TXTSave* object1, CSVSave* Object2) { // we assume that a file named UserInfo.csv and UserInfo.txt exist, they may be empty to show a new database 
-//TODO suffering out of range
+AccountList::AccountList (UserInfo* object1, UserInfo* Object2) { // we assume that a file named UserInfo.csv and UserInfo.txt exist, they may be empty to show a new database 
 // call Chloe's class to read the database file
-	AccountList_ = this;
-	rOne = object1;
-	rTwo = Object2; // NOTE: whenever a new file save type is added it must be included in the constructor and as a pushback here
-	//vector<vector<string>> v = rOne->ReadList();
-	
-	// vector<vector<string>> v  = {};
+ // NOTE: whenever a new file save type is added it must be included in the constructor and as a pushback here
 
-	vector<vector<string>> v = {{"s","1","apple","12345","5","4"},{"c","2","banana","123456","6","0"},{"s","3","carrot","123456","6","3"},{"c","4","donut","12345","5","0"},{"c","5","eggtart","123456","7","0"}};
-
-
-
-
-	cout << "list read" << endl;
 	std::vector< std::vector<string> >::const_iterator row;
-	if (!v.empty()){
- 		for (int row = 0; row < v.size(); ++row) {
-			auto it = v.at(row);
-			if (it.at(0)== "c") {//checking entry
-				CheckingAccountProxy* temp = new CheckingAccountProxy(std::stod(it.at(3)), std::stoi(it.at(1)), it.at(2),std::stoi(it.at(4)));
-				database->emplace(std::stoi(it.at(1)), temp); cout << "creating check" << endl; 
-			}	
-			else {// otherwise saving entry
-				SavingAccountProxy* Temp = new SavingAccountProxy(std::stod(it.at(3)), std::stoi(it.at(1)), it.at(2),std::stoi(it.at(4)),std::stod(it.at(5)));
-				database->emplace(std::stoi(it.at(1)),Temp); cout << "creating save" << endl;
-			}
-		}
-	} 
+
+  vector<vector<string>> v = object1->ReadList();
+
+  //vector<vector<string>> v = {{"s","1","apple","12345.000000","5.000000","4"},{"c","2","banana","123456.000000","6.000000","0"},{"s","3","carrot","123456.000000","6.000000","3"},{"c","4","donut","12345.000000","5.000000","0"},{"c","5","eggtart","123456.000000","7.000000","0"}};
+
+  cout << "read list" << endl;
+  
+  if (!v.empty()) {
+    for (int j = 0; j < (v.at(0)).size(); ++j) {
+      if((v.at(0)).at(j) == "c") {
+        CheckingAccountProxy* temp = new CheckingAccountProxy( stod((v.at(3)).at(j)), stoi((v.at(1)).at(j)), (v.at(2)).at(j), stod((v.at(4)).at(j)) );
+        database->emplace( stoi((v.at(1)).at(j)), temp );
+        cout << "creating check" << endl;
+      }
+      else {
+        SavingAccountProxy* Temp = new SavingAccountProxy ( stod((v.at(3)).at(j)), stoi((v.at(1)).at(j)), (v.at(2)).at(j), stoi((v.at(5)).at(j)), stod((v.at(4)).at(j)) );
+        database->emplace( stoi((v.at(1)).at(j)), Temp );
+        cout << "creating save" << endl;
+      }
+    }
+  }
+
+	// if (!v.empty()){
+ 	// 	for (int row = 0; row < v.size(); ++row) {
+	// 		auto it = v.at(row);
+	// 		if (it.at(0)== "c") {//checking entry
+  //       cout << "CHECK" << endl;
+	// 			CheckingAccountProxy* temp = new CheckingAccountProxy(stod(it.at(3)), stoi(it.at(1)), it.at(2),stod(it.at(4)));
+	// 			database->emplace(stoi(it.at(1)), temp); 
+  //       cout << "creating check" << endl; 
+	// 		}	
+	// 		else {// otherwise saving entry
+  //       cout << "SAVINGS" << endl;
+	// 			SavingAccountProxy* Temp = new SavingAccountProxy(stod(it.at(3)), stoi(it.at(1)), it.at(2), stoi(it.at(5)), stod(it.at(4))); 
+	// 			database->emplace(stoi(it.at(1)),Temp); 
+  //       cout << "creating save" << endl;
+	// 		}
+	// 	}
+	// }
 }
 
-AccountList::~AccountList() { //TODO Get this working suffering out of range
+AccountList::~AccountList() { //TODO Get this working
 // call Chloe's class to write the account data to the database file
 
-//cout << "we have reached the end times" <<endl;
+cout << "we have reached the end times" <<endl;
 // delete all pointers in the hash map
-		int i = 1;
-		int max = database->size();
-		while (i <= max) {
-	cout << "i is: " << i <<endl;		
-			cout << "we have "<<database->size()-i <<" more accounts to log" << endl;
-			auto x = database->at(i);
+	for (auto OBJECT : objects){
+		int i = 1; //NOTE
+		
+		while (!database->empty()) {
+			
+			 auto x = database->at(i);
 			i++;
-			cout << x->whatType()<< endl;
-			if (x->whatType() == 'c'){
-				cout << "submitting a checking account to save" << endl;
+			if (typeid(x).name() == "CheckingAccountProxy"){
 				CheckingAccountProxy* y = dynamic_cast<CheckingAccountProxy*>(x);
-				cout <<"expect c: "<< y->whatType()<< endl;
-				rOne->SaveInfo("c",y->getID(), y->getPassword(), y->getBalance(), y->getMonthlyHomeRent());
-				rTwo->SaveInfo("c",y->getID(), y->getPassword(), y->getBalance(), y->getMonthlyHomeRent());		
+				OBJECT->SaveInfo("c",y->getID(), y->getPassword(), y->getBalance(), y->getMonthlyHomeRent()); 
 			}
-			if (x->whatType() == 's'){//		NOTE: testing can't support this request at this time, we'll fo it later
-				cout << "submitting a saving account to save" << endl;
+			if (typeid(x).name() == "SavingAccountProxy"){//		NOTE: testing can't support this request at this time, we'll fo it later
 				SavingAccountProxy* z = dynamic_cast<SavingAccountProxy*>(x);
-				cout<<"expect s: " << z->whatType()<< endl;
-				rOne->SaveInfo("s",z->getID(), z->getPassword(), z->getBalance(), z->getStockNum(),z->getStockPrice() );
-				rTwo->SaveInfo("s",z->getID(), z->getPassword(), z->getBalance(), z->getStockNum(),z->getStockPrice() );
+        OBJECT->SaveInfo("s", z->getID(), z->getPassword(), z->getBalance(), z->getStockPrice(), z->getStockNum());
+
 			}
 			delete x;
-			
 
 		} //once objects in memory are deleted we can close the program
+	} // repeat for each type of save file
+ 
 //delete this; // this might be overkill or could somehow break the program? not realy sure
 }
 
 
-AccountList* AccountList::GetInstance(TXTSave* one, CSVSave* two) {
+AccountList* AccountList::GetInstance(UserInfo* one, UserInfo* two) {
 // check to see if an accountlist exists, if so return it 
 	if (AccountList_==nullptr) AccountList_ = new AccountList(one,two);
 	return AccountList_;
@@ -181,7 +188,6 @@ void AccountList::logIn() {
 	std::cin >> pass;
 	if (userPointer->getPassword() != pass) {std::cout << "password invalid, terminating"; return;}
 	userPointer->Menu();
-	userPointer = nullptr;
 }
 
 void AccountList::adminMenu() {
@@ -198,4 +204,12 @@ void AccountList::adminMenu() {
 	if (temp == '2') createAccount();
 	return;
 
+}
+
+void AccountList::logOut() {
+	// is called by account history proxy logout function
+	// unset user pointer
+	userPointer = NULL;
+	// re call the log in prompt
+	interface(); // TODO: verify the function of the interface and how we should return to it after we finish loggiing out  
 }
